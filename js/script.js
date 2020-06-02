dd1c878bd126a5f78a1698bdb931b05f42a3ca70
androszr <androszr@gmail.com>
2020-06-02 10:11:08 +0200

    Add handlebars for choose players

--- js/script.js
+++ js/script.js
@@ -1,13 +1,53 @@
 {
   'use strict';
 
-  class GameStart {
+  const select = {
+    startScreen: {
+      playersChoice: '.player-choice',
+      inputName: '.player-name-input',
+      inputEmail: '.player-email-input',
+      submitButton: '.start-game-form [type="submit"]'
+    },
+    
+  };
+
+  const classNames = {
+
+  }
+
+  const settings = {
+
+  }
+
+  const utils = {};
+
+  utils.createDOMFromHTML = function(htmlString) {
+  let div = document.createElement('div');
+  div.innerHTML = htmlString.trim();
+  return div.firstChild;
+
+};
+
+  const templates = {
+    playerChoice: Handlebars.compile(document.querySelector('#template-player-choice').innerHTML),
+  }
+
+  class GameInit {
     constructor(gameData) {
       this.data = gameData;
       this.renderPlayers();
     }
     renderPlayers() {
       console.log('Game data: ', this.data);
+      for (let player in this.data.players) {
+        console.log(this.data.players[player]);
+
+        const generatedHTML = templates.playerChoice(this.data.players[player]);
+        this.element = utils.createDOMFromHTML(generatedHTML);
+        const targetElement = document.querySelector(select.startScreen.playersChoice);
+        targetElement.appendChild(this.element);
+
+      }
     }
   }
   const app = {
@@ -18,7 +58,7 @@
         .then(response => response.json())
         .then(data => {
           this.data = data;
-          new GameStart(this.data);
+          new GameInit(this.data);
         });
     },
     init: function(){
