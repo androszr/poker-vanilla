67f83744b06d1bd595c3472ba8f1d0cd17883bd9
androszr <androszr@gmail.com>
2020-06-02 15:41:20 +0200

    Add first batch of validation for init game form. Add next batch of styling and elements to init game section

--- js/script.js
+++ js/script.js
@@ -3,30 +3,39 @@
 
   const select = {
     startScreen: {
-      playersChoice: '.player-choice',
       inputName: '.player-name-input',
-      inputEmail: '.player-email-input',
-      submitButton: '.start-game-form [type="submit"]'
+      consentField: '.terms-condition',
+      submitButton: '.start-game-form [type="submit"]',
+      playersChoice: '.player-choice',
+      playerAvatar: '.player-details',
+      allPlayers: '.player',
+      activePlayer: '.player-active',
+      currentBalance: '.balance-current p',
     },
     
   };
 
   const classNames = {
-
+    startScreen: {
+      activePlayer: 'player-active',
+      alertShow: 'input-alert-show',
+    },
   }
 
   const settings = {
-
+    validate: {
+      name: {
+        regex: /^[a-zA-Z0-9]{2,}$/,
+        errorAlertField: '.name-alert',
+      },
+      players: {
+        errorAlertField: '.player-alert',
+      },
+      consent: {
+        errorAlertField: '.consent-alert',
+      }
+    }
   }
-
-  const utils = {};
-
-  utils.createDOMFromHTML = function(htmlString) {
-  let div = document.createElement('div');
-  div.innerHTML = htmlString.trim();
-  return div.firstChild;
-
-};
 
   const templates = {
     playerChoice: Handlebars.compile(document.querySelector('#template-player-choice').innerHTML),
@@ -35,23 +44,94 @@
   class GameInit {
     constructor(gameData) {
       this.data = gameData;
+      this.currentActivePlayer = '';
       this.renderPlayers();
+      this.getElements();
+      this.initActions();
     }
+    getElements() {
+      this.elements = {};
+      this.elements.name = document.querySelector(select.startScreen.inputName);
+      this.elements.consent = document.querySelector(select.startScreen.consentField);
+      this.elements.formSubmit = document.querySelector(select.startScreen.submitButton);
+      this.elements.players = document.querySelector(select.startScreen.playersChoice);
+      this.elements.allPlayers = document.querySelectorAll(select.startScreen.allPlayers);
+
+      this.elements.nameError = document.querySelector(settings.validate.name.errorAlertField);
+      this.elements.playerError = document.querySelector(settings.validate.players.errorAlertField);
+      this.elements.consentError = document.querySelector(settings.validate.consent.errorAlertField);
+    };
     renderPlayers() {
       console.log('Game data: ', this.data);
       for (let player in this.data.players) {
-        console.log(this.data.players[player]);
-
         const generatedHTML = templates.playerChoice(this.data.players[player]);
-        this.element = utils.createDOMFromHTML(generatedHTML);
         const targetElement = document.querySelector(select.startScreen.playersChoice);
-        targetElement.appendChild(this.element);
+        targetElement.insertAdjacentHTML('beforeend', generatedHTML);
+        const playerBox = document.querySelector('.'+this.data.players[player].id)
 
+        playerBox.addEventListener('click', () => {
+          event.preventDefault();
+          this.playerChoice(this.data.players[player].id);
+        });
       }
-    }
+    };
+    playerChoice(playerId) {
+      this.currentActivePlayer = document.querySelector(select.startScreen.activePlayer);
+      if (this.currentActivePlayer) {
+        this.currentActivePlayer.classList.remove(classNames.startScreen.activePlayer, false)
+      }
+      this.currentActivePlayer = this.elements.players.querySelector('.'+playerId);
+      this.currentActivePlayer.classList.add(classNames.startScreen.activePlayer);
+    };
+    initActions() {
+      this.elements.formSubmit.addEventListener('click', () => {
+        event.preventDefault();
+        this.initGame();
+      });
+    };
+    validateName() {
+      const nameRegex = new RegExp(settings.validate.name.regex);
+      if (nameRegex.test(this.elements.name.value)) {
+        this.elements.nameError.classList.remove(classNames.startScreen.alertShow, false);
+        return true;
+      } else {
+        this.elements.nameError.classList.add(classNames.startScreen.alertShow);
+        return false;
+      }
+    };
+    validatePlayer() {
+      if (this.currentActivePlayer) {
+        this.elements.playerError.classList.remove(classNames.startScreen.alertShow, false);
+        return true;
+      } else {
+        this.elements.playerError.classList.add(classNames.startScreen.alertShow);
+        return false;
+      }
+    };
+    validateConsent() {
+
+    };
+    validate() {
+      const validateName = this.validateName();
+      const validatePlayer = this.validatePlayer();
+      if (!validateName || !validatePlayer) {
+        return false;
+      } else {
+        return true;
+      }
+      
+    };
+    initGame() {
+      console.log('current elements: ', this.elements);
+      console.log('current active player: ', this.currentActive);
+      if (!this.validate()) {
+        return;
+      }
+      console.log('validate passed');
+    };
   }
+
   const app = {
-    
     initData: function(){
       this.data = {};
       fetch('../db/data.json')
