70fe53e2e1efb43e694f23185cd9e0b9ff67f30e
androszr <androszr@gmail.com>
2020-06-05 14:02:47 +0200

    Add bet method to select random cards from card deck. Add functionality to select cards

--- js/components/GameSession.js
+++ js/components/GameSession.js
@@ -5,12 +5,17 @@ class GameSession {
     this.data = gameSessionData;
     this.getElements();
     this.initSession();
+    this.initActions();
+    this.gameStart = false;
   }
   getElements() {
     this.startSection = document.querySelector(select.startScreen.startSection);
     this.gameSection = document.querySelector(select.gameScreen.gameSection);
     this.avatarImgSection = document.querySelector(select.global.avatarImgWrapper);
     this.nameSection = document.querySelector(select.global.nameWrapper);
+    this.playWrapper = document.querySelector(select.gameScreen.playWrapper);
+    this.playerCards = document.querySelector(select.gameScreen.cardGrid).children;
+    this.choiceOptions = document.querySelectorAll(select.gameScreen.choiceAll);
   }
   initSession() {
     console.log('starting game session: ', this.data);
@@ -24,6 +29,88 @@ class GameSession {
     this.nameSection.classList.add(classNames.global.playerShow);
     this.avatarImgSection.src = this.avatarImgSection.src.replace(settings.default.playerAvatar, this.data.avatar);
     this.avatarImgSection.parentElement.classList.add(classNames.global.playerShow);
+  }
+  initActions() {
+    this.playWrapper.addEventListener('click', () => {
+      event.preventDefault();
+      this.playWrapperClicked('Space');
+    });
+    document.addEventListener('keydown', (e) => {
+      const cardOptions = settings.cardOptions;
+      if (cardOptions.includes(e.code)) {
+        const choiceDigit = e.code.charAt(e.code.length - 1);
+        this.chooseCard(choiceDigit);
+      } else if (e.code=='Space' || e.code=='Enter') {
+        this.playWrapperClicked(e.code);
+      }
+    });
+    for (let option of this.choiceOptions) {
+      option.addEventListener('click', () => {
+        const classList = event.target.className.split(' ');
+        const choiceDigit = classList[1].replace('choice-', '')
+        this.chooseCard(choiceDigit);
+      });
+    }
+    for (let card of this.playerCards) {
+      card.addEventListener('click', () => {
+        const classList = event.target.className.split(' ');
+        const choiceDigit = classList[1].replace('card-', '');
+        this.chooseCard(choiceDigit);
+      });
+    }
+  }
+  playWrapperClicked(code) {
+    if (code=='Enter' || code=='Space') {
+      this.playWrapper.classList.toggle(classNames.gameScreen.clickedPlayCard);
+      setTimeout(() => { 
+        this.playWrapper.classList.toggle(classNames.gameScreen.clickedPlayCard);
+      }, 200);
+      this.bet();
+    }
+  }
+  chooseCard(digit) {
+    if (this.gameStart) {
+      const choiceOption = document.querySelector(select.gameScreen.choiceCard.replace('*', digit));
+      const choosenCard = document.querySelector(select.gameScreen.card.replace('*', digit));
+      choiceOption.classList.toggle(classNames.gameScreen.clickedChoiceCard);
+      choosenCard.classList.toggle(classNames.gameScreen.choosenCard);
+    }
+  }
+  bet() {
+    this.gameStart = true;
+    this.cardsInBet = [...settings.allCards];
+    /* remove choosen cards from allCards */ 
+    console.log('cards before removing choosen: ', this.cardsInBet);
+    this.removeChoosenCardsFromBet();
+    console.log('cards after removing choosen: ', this.cardsInBet);
+    for (let card of this.playerCards) {
+      const cardDigit = this.getCardDigit(card);
+      if (!card.classList.contains(classNames.gameScreen.choosenCard)) {
+        const randomCard = this.cardsInBet[Math.floor(Math.random() * this.cardsInBet.length)];
+        const cardIndex = this.cardsInBet.indexOf(randomCard); 
+        if (cardIndex > -1) { 
+          this.cardsInBet.splice(cardIndex, 1);
+        }
+        card.setAttribute('data-value', randomCard);
+        card.innerHTML = settings.cardElement.replace(/\*/g, randomCard).replace('%', cardDigit);
+      }
+    }
+  }
+  getCardDigit(card) {
+    const classList = card.className.split(' ');
+    const choiceDigit = classList[1].replace('card-', '');
+    return choiceDigit;
+  }
+  removeChoosenCardsFromBet() {
+    for (let card of this.playerCards) {
+      if (card.classList.contains(classNames.gameScreen.choosenCard)) {
+        console.log('this card is chosen: ', card);
+        const cardIndex = this.cardsInBet.indexOf(card.getAttribute('data-value')); 
+        if (cardIndex > -1) { 
+          this.cardsInBet.splice(cardIndex, 1);
+        }
+      }
+    }
   }
 }
 
--- js/settings.js
+++ js/settings.js
@@ -16,7 +16,12 @@ export const select = {
     currentBalance: '.balance-current p',
   },
   gameScreen: {
+    cardGrid: '.card-grid',
     gameSection: '#game',
+    playWrapper: '.play-cards',
+    choiceCard: '.choice-*',
+    choiceAll: '.choice',
+    card: '.card-*',
   }
   
 };
@@ -29,7 +34,11 @@ export const classNames = {
   startScreen: {
     activeAvatar: 'avatar-active',
     alertShow: 'input-alert-show',
-    
+  },
+  gameScreen: {
+    clickedPlayCard: 'clicked-play-card',
+    clickedChoiceCard: 'choice-active',
+    choosenCard: 'card-choosen',
   },
 }
 
@@ -48,7 +57,26 @@ export const settings = {
   },
   default: {
     playerAvatar: 'player-default',
-  }
+  },
+  cardElement: '<img src="images/cards/*.png" alt="*" class="card-image card-%">',
+  cardOptions: [
+    'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5'
+  ],
+  allCards: [
+    '2-a', '2-b', '2-c', '2-d',
+    '3-a', '3-b', '3-c', '3-d',
+    '4-a', '4-b', '4-c', '4-d',
+    '5-a', '5-b', '5-c', '5-d',
+    '6-a', '6-b', '6-c', '6-d',
+    '7-a', '7-b', '7-c', '7-d',
+    '8-a', '8-b', '8-c', '8-d',
+    '9-a', '9-b', '9-c', '9-d',
+    '10-a', '10-b', '10-c', '10-d',
+    'J-a', 'J-b', 'J-c', 'J-d',
+    'Q-a', 'Q-b', 'Q-c', 'Q-d',
+    'K-a', 'K-b', 'K-c', 'K-d',
+    'A-a', 'A-b', 'A-c', 'A-d',
+  ]
 }
 
 export const templates = {
